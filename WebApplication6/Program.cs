007e250d6b5e628772225994fb2951177b8b1f89
Yashwanth192112019 <yashwanthmalladi9@gmail.com>
2025-07-24 17:05:22 +0530

    first commit

---
+++ WebApplication6/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+// here i have made the changes from the remote file
+//second change
+// Add services to the container.
+
+//a change done in the local repository.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
