f19bebd26d6a85d7ea72e4228ba5c5a1e59d0a18
RakshithaBS04 <rakshithabs2002@gmail.com>
2025-07-24 17:11:23 +0530

    first commit

---
+++ LoanTrial/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+    Console.WriteLine("This is a loan trial program."); 
+Console.WriteLine("Please enter the loan amount:");
+decimal loanAmount = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Please enter the interest rate (as a percentage):");
+decimal interestRate = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Please enter the number of years for the loan:");
+int years = Convert.ToInt32(Console.ReadLine());
+decimal monthlyRate = interestRate / 100 / 12;
+int months = years * 12;
+decimal monthlyPayment = (loanAmount * monthlyRate) / (1 - (decimal)Math.Pow((double)(1 + monthlyRate), -months));
+Console.WriteLine($"Your monthly payment is: {monthlyPayment:C2}");
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+
